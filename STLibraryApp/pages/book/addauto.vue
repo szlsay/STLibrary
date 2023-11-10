d0903a0931c16b88cfb67b551f7338ec844a3654
小沈曰 <409178030@qq.com>
2023-11-10 09:25:11 +0800

    book

---
+++ STLibraryApp/pages/book/addauto.vue
@@ -0,0 +1,61 @@
+<template>
+	<view class="page">
+		<view class="page-input">
+			<view class="input-code">
+				<uni-easyinput placeholder="请输入13位的ISBN~" maxlength="13" v-model="isbn" trim="both"></uni-easyinput>
+				<button type="primary" @click="search" style="width: 20%;">查询</button>
+			</view>
+
+		</view>
+		<view class="page-result" v-if="result">
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				isbn: '9787557010539',
+				result: null,
+			}
+		},
+		methods: {
+			search() {
+				if (this.isbn && this.isbn.length === 13) {
+					
+				} else {
+					uni.showToast({
+						title: '请输入13位的ISBN~',
+						icon: 'none'
+					});
+				}
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	::v-deep .uni-modal__textarea {
+		font-size: 36rpx;
+	}
+
+	.page {
+		padding: 40rpx;
+	}
+
+	.input-code {
+		display: flex;
+		align-items: center;
+		margin-bottom: 16rpx;
+
+		uni-button {
+			height: 72rpx;
+			margin-left: 16rpx;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			background-color: #00cc99;
+		}
+	}
+</style>
--- STLibraryApp/pages/home/home.vue
+++ STLibraryApp/pages/home/home.vue
@@ -1,36 +1,89 @@
 <template>
-	<view>
-		123
-		{{text}}
+	<view class="page-home">
+		<view class="st-box" v-if="menusInput && menusInput.length > 0">
+			<view class="title">
+				数据导入
+			</view>
+			<view class="list">
+				<view class="item" v-for="(item, index) in menusInput" :key="index" @click="onClickItem(item)">
+					<image :src="item.img"></image>
+					<text class="text">{{item.text}}</text>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
 <script>
+	import {
+		mutations
+	} from '@/uni_modules/uni-id-pages/common/store.js'
 	export default {
 		data() {
 			return {
-				text: null
+				menusInput: [{
+					"img": "/static/home/book-add.png",
+					"text": "添加书籍",
+					"to": "/pages/book/addauto",
+				}]
 			}
 		},
-		onShow() {
-			this.getData()
-		},
 		methods: {
-			getData() {
-				uni.request({
-					url: 'http://47.99.80.202:6066/openApi/getInfoByIsbn?isbn=9787115461476&appKey=ae1718d4587744b0b79f940fbef69e77',
-					success: (res) => {
-						const {data} = res
-						if (data) {
-							this.text = data.data
-						}
-					}
-				});
+			onClickItem(item) {
+				if (item.to) {
+					uni.navigateTo({
+						url: item.to
+					})
+				}
 			}
 		}
 	}
 </script>
 
-<style>
+<style lang="scss" scoped>
+	.page-home {
+		background-color: #00CC9933;
+		padding: 32rpx;
+		height: calc(100vh);
+		overflow-y: auto;
+	}
 
+	.st-box {
+		background-color: white;
+		border-radius: 16rpx;
+		margin-bottom: 32rpx;
+		padding: 40rpx;
+
+		.title {
+			font-weight: 800;
+			font-size: 36rpx;
+		}
+
+		.list {
+			display: flex;
+			flex-wrap: wrap;
+			align-items: center;
+
+			.item {
+				padding-top: 32rpx;
+				width: 25%;
+				display: flex;
+				flex-direction: column;
+				justify-content: center;
+				align-items: center;
+
+				image {
+					width: 100rpx;
+					height: 100rpx;
+				}
+
+				.text {
+					padding-top: 16rpx;
+					color: #333;
+					font-size: 28rpx;
+				}
+
+			}
+		}
+	}
 </style>
