e9b4c7b9c8c2cc74f9352f7e7e12f700ee0866f3
小沈曰 <409178030@qq.com>
2023-11-13 11:01:51 +0800

    book

--- STLibraryApp/pages/book/addauto.vue
+++ STLibraryApp/pages/book/addauto.vue
@@ -12,7 +12,7 @@
 					<uni-easyinput placeholder="请输入ISBN" v-model="formData.isbn" disabled></uni-easyinput>
 				</uni-forms-item>
 				<uni-forms-item name="bookName" label="书名">
-					<uni-easyinput type="textarea" placeholder="请输入书名" v-model="formData.bookName" disabled></uni-easyinput>
+					<uni-easyinput placeholder="请输入书名" v-model="formData.bookName" disabled></uni-easyinput>
 				</uni-forms-item>
 				<uni-forms-item name="author" label="作者">
 					<uni-easyinput placeholder="请输入作者" v-model="formData.author" disabled></uni-easyinput>
@@ -20,14 +20,14 @@
 				<uni-forms-item name="bookDesc" label="介绍">
 					<uni-easyinput type="textarea" placeholder="请输入内容介绍" v-model="formData.bookDesc" disabled></uni-easyinput>
 				</uni-forms-item>
-				<view class="book-box">
+				<!-- 				<view class="book-box">
 					<uni-forms-item name="clcCode" label="类型" style="width: 300rpx;">
 						<uni-easyinput placeholder="请输入类型编码" v-model="formData.clcCode" disabled></uni-easyinput>
 					</uni-forms-item>
 					<uni-forms-item name="clcName" style="flex: 1; margin-left: -60rpx;">
 						<uni-easyinput placeholder="请输入类型名称" v-model="formData.clcName" disabled></uni-easyinput>
 					</uni-forms-item>
-				</view>
+				</view> -->
 				<view class="book-box">
 					<uni-forms-item name="press" label="出版社" style="width: 300rpx;">
 						<uni-easyinput placeholder="请输入出版社" v-model="formData.press" disabled></uni-easyinput>
@@ -36,9 +36,9 @@
 						<uni-easyinput placeholder="请输入出版时间" v-model="formData.pressDate" disabled></uni-easyinput>
 					</uni-forms-item>
 				</view>
-				<uni-forms-item name="pressPlace" label="出版地址">
+				<!-- 				<uni-forms-item name="pressPlace" label="出版地址">
 					<uni-easyinput placeholder="请输入出版地址" v-model="formData.pressPlace" disabled></uni-easyinput>
-				</uni-forms-item>
+				</uni-forms-item> -->
 				<uni-forms-item name="pictures" label="图片">
 					<image v-for="(url, index) in formData.pictures" :key="index" :src="url" mode="aspectFill"
 						@tap.native.stop="onClickPreview(url)"></image>
@@ -149,8 +149,9 @@
 					uni.showLoading({
 						mask: true
 					})
+
 					uni.request({
-						url: `http://47.99.80.202:6066/openApi/getInfoByIsbn?isbn=${this.isbn}&appKey=ae1718d4587744b0b79f940fbef69e77`,
+						url: `https://api.jike.xyz/situ/book/isbn/${this.isbn}?apikey=12524.6833bc5d3ba860a9242c196b4feaae26.8da93d6d3ac6f6bc9f2ab633b26cc791`,
 						success: (res) => {
 							uni.hideLoading()
 							const {
@@ -158,17 +159,19 @@
 							} = res
 							if (data && data.data) {
 								let tempData = data.data
-								if (tempData.pictures) {
-									const result = JSON.parse(tempData.pictures);
-									if (result && Array.isArray(result) && result.length > 0) {
-										tempData.pictures = result.map(item => {
-											if (item.indexOf('douban') != -1) {
-												item = `https://images.weserv.nl/?url=${item}`
-											}
-											return item
-										})
+								tempData.bookDesc = tempData.description
+								tempData.bookName = tempData.name
+								tempData.isbn = tempData.code
+
+								if (tempData.photoUrl) {
+									let photoUrl = tempData.photoUrl
+									if (tempData.photoUrl.indexOf('douban') != -1) {
+										photoUrl = `https://images.weserv.nl/?url=${tempData.photoUrl}`
 									}
+									tempData.pictures = [photoUrl]
 								}
+								tempData.press = tempData.publishing
+								tempData.pressDate = tempData.published
 								this.formData = tempData
 							} else {
 								uni.showModal({
@@ -185,6 +188,43 @@
 							})
 						}
 					});
+
+					// uni.request({
+					// 	url: `http://47.99.80.202:6066/openApi/getInfoByIsbn?isbn=${this.isbn}&appKey=ae1718d4587744b0b79f940fbef69e77`,
+					// 	success: (res) => {
+					// 		uni.hideLoading()
+					// 		const {
+					// 			data
+					// 		} = res
+					// 		if (data && data.data) {
+					// 			let tempData = data.data
+					// 			if (tempData.pictures) {
+					// 				const result = JSON.parse(tempData.pictures);
+					// 				if (result && Array.isArray(result) && result.length > 0) {
+					// 					tempData.pictures = result.map(item => {
+					// 						if (item.indexOf('douban') != -1) {
+					// 							item = `https://images.weserv.nl/?url=${item}`
+					// 						}
+					// 						return item
+					// 					})
+					// 				}
+					// 			}
+					// 			this.formData = tempData
+					// 		} else {
+					// 			uni.showModal({
+					// 				content: '未获取到书本信息',
+					// 				showCancel: false
+					// 			})
+					// 		}
+					// 	},
+					// 	fail: (err) => {
+					// 		uni.hideLoading()
+					// 		uni.showModal({
+					// 			content: err.message || '请求服务失败',
+					// 			showCancel: false
+					// 		})
+					// 	}
+					// });
 				} else {
 					uni.showToast({
 						title: '请输入13位的ISBN~',
